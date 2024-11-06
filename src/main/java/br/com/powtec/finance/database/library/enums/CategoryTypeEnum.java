a5a8c734d56c753637f95c82f31ef8d9d3e244b6
Eduardo <eduardo.a3@icloud.com>
2024-11-05 22:27:09 -0300

    Includded all categories

--- src/main/java/br/com/powtec/finance/database/library/enums/CategoryTypeEnum.java
+++ src/main/java/br/com/powtec/finance/database/library/enums/CategoryTypeEnum.java
@@ -2,16 +2,12 @@ package br.com.powtec.finance.database.library.enums;
 
 public enum CategoryTypeEnum {
 
-    INVESTMENT("INVESTMENT"),
-    FOOD("FOOD"),
-    CARD("CARD"),
-    TRANSPORT("TRANSPORT"),
-    HEALTH("HEALTH"),
-    MONTHLY_EXPENSES("HEALTH");
-
-    private String name;
-
-    CategoryTypeEnum (String name) {
-        this.name = name;
-    }
+    INVESTMENT,
+    FOOD,
+    CARD,
+    TRANSPORT,
+    HEALTH,
+    MONTHLY_EXPENSES,
+    VEHICLES,
+    TRIP;
 }
