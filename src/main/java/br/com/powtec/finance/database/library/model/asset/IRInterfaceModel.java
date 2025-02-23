d03e9c86779cd4ec93a0509804d8e0c81906e852
Eduardo <eduardo.silva@icloud.com>
2025-02-23 17:18:53 -0300

    IR feature

---
+++ src/main/java/br/com/powtec/finance/database/library/model/asset/IRInterfaceModel.java
@@ -0,0 +1,19 @@
+package br.com.powtec.finance.database.library.model.asset;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class IRInterfaceModel {
+    Double averagePrice;
+    Double totalValue;
+    Double totalAmount;
+    Double totalValueLastYear;
+}
--- src/main/java/br/com/powtec/finance/database/library/repository/AssetRepository.java
+++ src/main/java/br/com/powtec/finance/database/library/repository/AssetRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.powtec.finance.database.library.enums.AssetTypeEnum;
@@ -11,5 +13,49 @@ import br.com.powtec.finance.database.library.model.AssetModel;
 
 @Repository
 public interface AssetRepository extends JpaRepository<AssetModel, Long>, JpaSpecificationExecutor<AssetModel> {
-  public List<AssetModel> findAllByType(AssetTypeEnum type);
+  List<AssetModel> findAllByType(AssetTypeEnum type);
+
+  @Query("""
+      SELECT SUM(m.amount * m.unitValue) / SUM(m.amount)
+      FROM AssetMovementModel m
+      WHERE m.asset.id = :id
+      AND m.operation = 'BUY'
+      AND YEAR(m.date) <= :year
+      """)
+  Double calcAveragePrice(@Param("id") Long id, @Param("year") Integer year);
+
+  @Query("""
+      SELECT SUM(
+        CASE
+          WHEN m.operation = 'BUY' THEN m.amount
+          ELSE m.amount * -1
+        END
+      ) AS current_amount
+      FROM AssetMovementModel m
+      WHERE m.asset.id = :id
+      AND YEAR(m.date) <= :year
+      """)
+  Double calcCurrentAmount(@Param("id") Long id, @Param("year") Integer year);
+
+  @Query("""
+      SELECT SUM(m.amount * m.unitValue) / SUM(m.amount)
+      FROM AssetMovementModel m
+      WHERE m.asset.id = :id
+      AND m.operation = 'BUY'
+      AND YEAR(m.date) <= (:year - 1)
+      """)
+  Double calcAveragePriceYearBefore(@Param("id") Long id, @Param("year") Integer year);
+
+  @Query("""
+      SELECT SUM(
+        CASE
+          WHEN m.operation = 'BUY' THEN m.amount
+          ELSE m.amount * -1
+        END
+      ) AS current_amount
+      FROM AssetMovementModel m
+      WHERE m.asset.id = :id
+      AND YEAR(m.date) <= (:year -1)
+      """)
+  Double calcCurrentAmountYearBefore(@Param("id") Long id, @Param("year") Integer year);
 }
